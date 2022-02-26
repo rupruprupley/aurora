db8add83b82589d718b6be04dc40d36a60367e5b
Jade Rupley <33669488+rupruprupley@users.noreply.github.com>
2022-02-26 00:02:51 -0800

    initial commit

---
+++ jsAurora.js
@@ -0,0 +1,19 @@
+//file name: jsAurora
+//file type: .js
+//create date: 2022-02-24
+//version: 0.0
+
+//toc
+//-- testing
+//-- visual
+//-- binary
+
+//section: testing
+//last revision: 2022-02-25
+console.log('It works!');
+
+//section: visual
+//last revision: 2022-02-25
+
+//section: binary
+//last revision: 2022-02-25
